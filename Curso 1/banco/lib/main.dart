f131a8cf7d675529556df4e6b1f58658e4f596e0
heron03 <heronze@gmail.com>
2020-03-06 13:02:23 -0300

    Video 1

--- Curso 1/banco/lib/main.dart
+++ Curso 1/banco/lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-	Column(
-		children: <Widget>[
-			Text('Ola', textDirection: TextDirection.ltr,),
-			Text('Mundo', textDirection: TextDirection.ltr,),
-			Expanded(
-			child: FittedBox(
-				fit: BoxFit.contain, // otherwise the logo will be tiny
-				child: const FlutterLogo(),
+	MaterialApp(
+		home: Scaffold(
+			appBar: AppBar(title: Text("Transferências"),),
+			floatingActionButton: FloatingActionButton(
+				child: Icon(Icons.add), onPressed: () {},
 			),
-			),
-		],
-	) 
+		),
+	),
 );
