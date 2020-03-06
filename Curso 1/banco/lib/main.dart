b088a88b6b4d6a9c915f80d184a3dc1d559de935
heron03 <heronze@gmail.com>
2020-03-06 13:55:17 -0300

    Video 6

--- Curso 1/banco/lib/main.dart
+++ Curso 1/banco/lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
 	MaterialApp(
 		home: Scaffold(
-			body: Column(
-				children: <Widget>[
-					Card(
-						child: ListTile(
-							leading: Icon(Icons.monetization_on),
-							title: Text('100.00'),
-							subtitle: (Text("1000")),
-						),
-					),
-					Card(
-						child: ListTile(
-							leading: Icon(Icons.monetization_on),
-							title: Text('200.00'),
-							subtitle: (Text("1100")),
-						),
-					),
-				],
-			),
+			body: ListaTrasnferencias(),
 			appBar: AppBar(title: Text("Transferências"),),
 			floatingActionButton: FloatingActionButton(
 				child: Icon(Icons.add), onPressed: () {},
@@ -28,3 +11,44 @@ void main() => runApp(
 		),
 	),
 );
+
+
+class ListaTrasnferencias extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+    
+	return Column(
+		children: <Widget>[
+			IntemTransferencia(Transferencia(100.00, 10000)),
+			IntemTransferencia(Transferencia(200.00, 20000)),
+			IntemTransferencia(Transferencia(400.00, 40000)),
+			IntemTransferencia(Transferencia(500.00, 50000)),
+		],
+	);
+	}
+}
+
+class IntemTransferencia extends StatelessWidget {
+    final Transferencia _transferencia;
+
+	IntemTransferencia(this._transferencia);
+	
+	@override
+	Widget build(BuildContext context) {
+
+	return Card(
+		child: ListTile(
+			leading: Icon(Icons.monetization_on),
+			title: Text(_transferencia.valor.toString()),
+			subtitle: (Text(_transferencia.numeroConta.toString())),
+		),
+	);
+	}
+}
+
+class Transferencia {
+	final double valor;
+	final int numeroConta;
+
+	Transferencia(this.valor, this.numeroConta);
+}
