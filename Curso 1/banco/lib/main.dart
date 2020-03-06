3680fe89f45453495102bb9200081cb625c7c96f
heron03 <heronze@gmail.com>
2020-03-06 11:48:18 -0300

    Widget video 7

--- Curso 1/banco/lib/main.dart
+++ Curso 1/banco/lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text("Ola mundo", textDirection: TextDirection.ltr,));
+void main() => runApp(
+	Column(
+		children: <Widget>[
+			Text('Ola', textDirection: TextDirection.ltr,),
+			Text('Mundo', textDirection: TextDirection.ltr,),
+			Expanded(
+			child: FittedBox(
+				fit: BoxFit.contain, // otherwise the logo will be tiny
+				child: const FlutterLogo(),
+			),
+			),
+		],
+	) 
+);
