a2e5927ab7e79bac90108d56c1e3a0842a91b514
heron03 <heronze@gmail.com>
2020-03-06 13:32:14 -0300

    Video 4

--- Curso 1/banco/lib/main.dart
+++ Curso 1/banco/lib/main.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(
 	MaterialApp(
 		home: Scaffold(
+			body: Column(
+				children: <Widget>[
+					Card(
+						child: ListTile(
+							leading: Icon(Icons.monetization_on),
+							title: Text('100.00'),
+							subtitle: (Text("1000")),
+						),
+					),
+					Card(
+						child: ListTile(
+							leading: Icon(Icons.monetization_on),
+							title: Text('200.00'),
+							subtitle: (Text("1100")),
+						),
+					),
+				],
+			),
 			appBar: AppBar(title: Text("Transferências"),),
 			floatingActionButton: FloatingActionButton(
 				child: Icon(Icons.add), onPressed: () {},
