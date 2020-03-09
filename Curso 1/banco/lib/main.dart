8b6c99d66c3ed0f9062dfa2c065f6f58c756ab7c
heron03 <heronze@gmail.com>
2020-03-09 10:53:39 -0300

    Video 5

--- Curso 1/banco/lib/main.dart
+++ Curso 1/banco/lib/main.dart
@@ -6,9 +6,15 @@ class Banco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-		home: Scaffold(
-			body: ListaTrasnferencias(),
-		),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary
+        )
+      ),
+		  home: ListaTrasnferencias(),
 	);
   }
 }
