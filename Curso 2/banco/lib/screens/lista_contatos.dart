149a956bee6fb664cc4d93aa9158a4a16a586d9d
heron03 <heronze@gmail.com>
2020-03-10 09:03:05 -0300

    video 1

--- Curso 2/banco/lib/screens/lista_contatos.dart
+++ Curso 2/banco/lib/screens/lista_contatos.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:banco/screens/formulario_contato.dart';
+import 'package:banco/models/contato.dart';
 
 class ListaContatatos extends StatelessWidget {
+
+  final List<Contato> contatos = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contatos'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alex',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contato contato = contatos[index];
+
+          return _ContatoItem(contato);
+        },
+        itemCount: contatos.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -37,6 +33,28 @@ class ListaContatatos extends StatelessWidget {
               );
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContatoItem extends StatelessWidget {
+  final Contato contato;
+
+  _ContatoItem(this.contato);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contato.nome,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contato.numeroConta.toString(),
+          style: TextStyle(fontSize: 24.0),
+        ),
       ),
     );
   }
