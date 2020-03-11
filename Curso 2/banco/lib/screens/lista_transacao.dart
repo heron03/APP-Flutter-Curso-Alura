345868961fe1d465c66a5b9e931f73fe26f2f233
heron03 <heronze@gmail.com>
2020-03-11 11:29:25 -0300

    Video 3

--- Curso 2/banco/lib/screens/lista_transacao.dart
+++ Curso 2/banco/lib/screens/lista_transacao.dart
@@ -1,38 +1,56 @@
+import 'package:banco/components/Progress.dart';
+import 'package:banco/http/webclient.dart';
 import 'package:banco/models/contato.dart';
 import 'package:flutter/material.dart';
 
 class ListaTransacao extends StatelessWidget {
-  final List<Transacao> transacaos = List();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transacaos'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transacao transacao = transacaos[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transacao.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transacao.contato.numeroConta.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      body: FutureBuilder<List<Transacao>>(
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Progress();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Transacao> transacaos = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Transacao transacao = transacaos[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(Icons.monetization_on),
+                      title: Text(
+                        transacao.value.toString(),
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        transacao.contato.numeroConta.toString(),
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: transacaos.length,
+              );
+              break;
+          }
+          return Text('Ocorreu um Erro desconhecido no APP');
         },
-        itemCount: transacaos.length,
       ),
     );
   }
@@ -51,5 +69,4 @@ class Transacao {
   String toString() {
     return 'Transacao{value: $value, contato: $contato}';
   }
-
 }
