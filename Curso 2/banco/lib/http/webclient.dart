c5964c5c62064a46795410655a634f1319d32c5f
heron03 <heronze@gmail.com>
2020-03-11 09:56:08 -0300

    Video 6

--- Curso 2/banco/lib/http/webclient.dart
+++ Curso 2/banco/lib/http/webclient.dart
@@ -1,6 +1,27 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoginInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('url ${data.url}');
+    print('headers ${data.headers}');
+    print('body ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+        print('url ${data.url}');
+    print('headers ${data.headers}');
+    print('body ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get('http://10.0.0.103:8080/transactions');
-  print(response.body);
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoginInterceptor()],
+  );
+  final Response response = await client.get('http://10.0.0.103:8080/transactions');
 }
